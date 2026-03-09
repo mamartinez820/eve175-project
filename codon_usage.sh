df0e8560a23e31d87f3877126f9f4fb4b2927643
Manuel Martinez <mamartine@ucdavis.edu>
2026-03-08 17:49:15 -0700

    Implement codon counting from FASTA sequences

--- codon_usage.sh
+++ codon_usage.sh
@@ -29,3 +29,62 @@ case "$fasta_file" in
         exit 1
         ;;
 esac
+
+awk '
+BEGIN {
+    total_codons = 0
+    seq = ""
+}
+
+# Header line indicates a new sequence
+/^>/ {
+
+#If seq isn't empty, then count its codon befeor reseting it to a empty string
+    if (seq != "") {
+
+#Find what part of the sequence is usable (no overhangs)
+        len = length(seq)
+        usable_len = len - (len % 3)
+
+#Loop through the sequence in intervals of 3 to find codons
+        for (i = 1; i <= usable_len; i += 3) {
+            codon = substr(seq, i, 3)
+            counts[codon]++
+            total_codons++
+        }
+
+#Reset seq to a empty string 
+        seq = ""
+    }
+    next
+}
+
+#If we are on a sequence line, add it to seq
+{
+    seq = seq toupper($0)
+}
+
+END {
+
+#Process final sequence using same algorithm
+    if (seq != "") {
+        len = length(seq)
+        usable_len = len - (len % 3)
+
+        for (i = 1; i <= usable_len; i += 3) {
+            codon = substr(seq, i, 3)
+            counts[codon]++
+            total_codons++
+        }
+    }
+
+
+#Print codons with nonzero counts
+    for (codon in counts) {
+        freq = counts[codon] / total_codons
+        printf "%s\t%d\t%.4f\n", codon, counts[codon], freq
+    }
+
+    printf "\nTotal codons: %d\n", total_codons
+}
+' "$fasta_file" | sort
